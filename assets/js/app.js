7f3ce3e35af35a8adc883396dbc2c22840ed3841
Polansia <62407108+Polansia@users.noreply.github.com>
2020-06-29 15:53:25 -0400

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+onload({
+
+
+
+	
+})
+
+
+
+function startGif(x) {
+	
+	x.src = "./assets/images/" + x.id + ".gif";
+
+}
+function endGif(x){
+
+
+	x.src = "./assets/images/" + x.id + ".JPG";
+
+}
